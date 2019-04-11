b7b8bf681871b414242415371fd8550186207d03
Luca Minotti <luca@osx.station>
2019-04-11 20:48:11 +0200

    modifica GPS_Communication

--- source/sensor/GPS_Communication.py
+++ source/sensor/GPS_Communication.py
@@ -3,18 +3,50 @@ class GPS_Communication():
     def __init__(self):
         pass
 
+    @abstractmethod
+    def poll(self):
+        pass
+    
+    @abstractmethod
+    def get(self):
+        pass
+
 class GPS_SPI(GPS_Communication):
     def __init__(self):
+        pass
+
+    def poll(self):
+        pass
+    
+    def get(self):
         pass
 
 class GPS_Serial(GPS_Communication):
     def __init__(self):
         pass
 
+    def poll(self):
+        pass
+    
+    def get(self):
+        pass
+
 class GPS_Daemon(GPS_Communication):
     def __init__(self):
         pass
 
+    def poll(self):
+        pass
     
+    def get(self):
+        pass
 
+class GPS_Dummy(GPS_Communication):
+    def __init__(self):
+        pass
 
+    def poll(self):
+        pass
+    
+    def get(self):
+        pass
