f3fc3ae561df22ccbe2e9e96459ee22a8a4c253c
Luca <>
2022-01-10 21:39:52 +0100

    cambiata dimensione logfile servizi in /etc/system.d/system (circa)

--- Core/Collector/Snap.py
+++ Core/Collector/Snap.py
@@ -7,6 +7,8 @@ class Snapshot:
         self.htp_data = ""
         self.pm_data = ""
         self.di_data = ""
+        self.file_size = 0
+        self.file = ""
 
     def clean_snap(self):
         self.gps_data = ""
@@ -30,5 +32,25 @@ class Snapshot:
         pass
 
     def take_snap(self):
-        print(f"SNAP: {self.gps_data} {self.htp_data} {self.pm_data}")
-        pass
+        sentence = f"{self.gps_data} {self.htp_data} {self.pm_data}"
+
+        if self.file_size == 0:
+            self.open_file()
+
+        self.write_file(sentence)
+
+        print(f"SNAP: {sentence}")
+
+    def write_file(self, s):
+        self.file.write(s+'\n')
+        self.file_size += len(s) + 1
+
+        if self.file_size > 15360:
+            self.close_file()
+            self.file_size = 0
+
+    def close_file(self):
+        self.file.close()
+
+    def open_file(self):
+        self.file = open("/home/debian/centralina/records/record_"+self.gps_data[0:17]+".csv", "w", buffering=1)
--- driver.py
+++
@@ -1,5 +0,0 @@
-from source.zOld2 import module12
-
-if __name__ == "__main__":
-    m=module12()
-    m.name()
